29fa90490bf047e9da034f1574b56c150677595d
armr1020 <arnab.mridha@netcracker.com>
2024-09-30 13:59:41 +0530

    fresh changes

---
+++ src/constructors/Performance.java
@@ -0,0 +1,16 @@
+package constructors;
+
+public class Performance {
+    public int marks = 50;
+    public double psp = 81.7;
+
+    public Performance() {
+        this.marks = 50;
+        this.psp = 81.7;
+    }
+
+    public Performance(Performance performance) {
+        this.marks = performance.marks;
+        this.psp = performance.psp;
+    }
+}
